cbc0fb3ce7da247ca59d195649d7cb29edf3ef17
Mohamad Keramatifar <info@keramatifar.ir>
2015-10-22 17:57:22 +0330

    Databasae Handler Added

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'softjob');
+    define('PDO_DSN', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE);
+?>
